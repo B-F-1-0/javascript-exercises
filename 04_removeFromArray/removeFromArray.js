40a44ccc1d295a1e63fdd68a56fc6ffa5d0d74b2
B-F-1-0 <jluc1485410@gmail.com>
2024-09-06 14:25:30 -0400

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value) { // ... specifies we can take an unlimited number of parameters and store it into an array
+    let newArray = array;
+
+    for (const params in value)
+    {
+        let i = 0;
+        while (i < newArray.length)
+        {
+            if (newArray[i] === value[params])
+            {
+                newArray.splice(i,1); // remove the found index if the item was found
+            }
+            else
+            {
+                ++i;
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
