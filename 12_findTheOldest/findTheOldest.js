487141478b6a96446d6c28885f28fe0590858eb0
B-F-1-0 <jluc1485410@gmail.com>
2024-11-13 12:36:49 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let startyear;
+    let deathyear;
+    let indexOfOldest = 0;
+    let ageOfOldest = 0;
+    people.forEach((person, index) => {
+        if (person.yearOfDeath == undefined)
+        {
+            deathyear = new Date().getFullYear();
+        }
+        else
+        {
+            deathyear = person.yearOfDeath;
+        }
+        startyear = person.yearOfBirth;
+        if ((deathyear - startyear) > ageOfOldest)
+        {
+            indexOfOldest = index;
+            ageOfOldest = (deathyear - startyear);
+        }
+    })
+    return people[indexOfOldest];
 };
 
 // Do not edit below this line
