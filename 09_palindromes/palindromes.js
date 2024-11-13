a92f18af04efa36c0166c0871412102fce95b01b
B-F-1-0 <jluc1485410@gmail.com>
2024-11-13 10:17:07 -0500

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (givenString) {
+    let normalString = givenString;
 
+    // cleanup string
+    normalString = normalString.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); // remove special characters
+    normalString = normalString.replaceAll(" ",""); // remove whitespace
+    normalString = normalString.toLowerCase();
+
+    // reverse the string
+    let reversedString = "";
+    for (let i = normalString.length - 1; i >= 0; i--)
+    {
+        reversedString = reversedString.concat(normalString[i]);
+    }
+
+    if (reversedString == normalString)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
