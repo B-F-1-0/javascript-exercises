b3e9b3f054a2a04ee288545da8d487b57ab87129
B-F-1-0 <jluc1485410@gmail.com>
2024-11-13 09:12:10 -0500

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function(digit1, digit2) {
+  digit1 += digit2;
+  return digit1;
 };
 
-const subtract = function() {
-	
+const subtract = function(digit1, digit2) {
+	digit1 -= digit2;
+  return digit1;
 };
 
-const sum = function() {
-	
+const sum = function(numberarray) {
+	if (numberarray.length == 0)
+  {
+    return 0;
+  }
+  else 
+  {
+    let temp = 0;
+    numberarray.forEach((element) => {temp += element}); // for each element in the array, add it to temp
+
+    return temp;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numberarray) {
+    if (numberarray.length == 0)
+    {
+      return 0;
+    }
+    else 
+    {
+      let temp = 1; // set to 1 so base number can be multiplied properly
+      numberarray.forEach((element) => {temp *= element}); // for each element in the array, multiply it by temp
+  
+      return temp;
+    }
 };
 
-const power = function() {
-	
+const power = function(digit1, digit2) {
+	digit1 = digit1 ** digit2;
+  return digit1;
 };
 
-const factorial = function() {
-	
+const factorial = function(startnumber) {
+	if (startnumber == 0)
+  {
+    return 1;
+  }
+  
+  let temp = startnumber;
+  for (let i = startnumber - 1; i > 0; i--)
+  {
+    temp *= i;
+  }
+  return temp;
 };
 
 // Do not edit below this line
